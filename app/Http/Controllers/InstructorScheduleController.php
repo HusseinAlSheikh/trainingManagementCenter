d1c67911ce2842df454d7c7c4e8ab0647fef0b81
husseinAlsheikh <hussein.jodd@gmail.com>
2024-03-22 22:30:16 +0300

    course managment 1

---
+++ app/Http/Controllers/InstructorScheduleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InstructorSchedule;
+use Illuminate\Http\Request;
+
+class InstructorScheduleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\InstructorSchedule  $instructorSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InstructorSchedule $instructorSchedule)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\InstructorSchedule  $instructorSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InstructorSchedule $instructorSchedule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\InstructorSchedule  $instructorSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InstructorSchedule $instructorSchedule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InstructorSchedule  $instructorSchedule
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InstructorSchedule $instructorSchedule)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::middleware(['guest'])->group(function () {
 
